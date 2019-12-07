d6e1fe7b013ac162bb62c2c2e1e25c9d8f51b352
leona <leonardodasilva.ifrn@gmail.com>
2019-12-07 16:00:44 -0300

    ex01

---
+++ ex01/ex01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+interface ITest
+{
+    int Metodo1(int v);
+}
+class Erro : ApplicationException { }
+class Teste : ITest
+{
+    public int Metodo1(int v)
+    {
+        if (v < 0) throw new Erro();
+        else return v;
+    }
+}
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Teste i = new Teste();
+        bool g = true;
+        while (g)
+        {
+            try
+            {
+                Console.WriteLine("Digite um numero positivo");
+                Console.WriteLine(i.Metodo1(int.Parse(Console.ReadLine())));
+                break;
+            }
+            catch (Erro t)
+            {
+                Console.WriteLine("isso é um numero negativo");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
